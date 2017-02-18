884768660c9546db5f99a096a5b3fa46b46c3fb5
aitthinat <aitthinat_s@cmu.ac.th>
2017-02-19 01:47:41 +0700

    Add files via upload

---
+++ accept_adddata.php
@@ -0,0 +1,18 @@
+<?php include "head.php" ?>
+<div style="background:#86e8db">
+    <div class="container" style="background:#f7fffd">
+        <div class="row">
+            <div class="col-lg-12">
+                <div style="width:100%" >
+                    <div align="center" style="padding-top:10px;padding-bottom:20px"><h1>ตรวจสอบข้อมูลการบันทึกข้อมูลการตรวจสุขภาพ</h1></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<?php include "foot.php" ?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include "head.php" ?>
+<div style="background:#d8e9f3">
+<div class="container">
+  <div class="row">
+    <div class="col align-self-start" style="background:black;width:">
+      <div><img class="img-responsive" src="img/rzm-61.png" alt="" height="200" width="200"></div>
+      <div><img class="img-responsive" src="img/rzm-6.png" alt="" height="200" width="200"></div>
+    </div>
+    <div class="col align-self-center" style="background:white">
+    <img class="img-responsive" src="img/home.png" alt=""  height="100%" width="100%">
+    </div>
+    <div class="col align-self-end" style="background:black">
+      <div><img class="img-responsive" src="img/rzm-62.png" alt="" height="200" width="200"></div>
+      <div><img class="img-responsive" src="img/rzm-63.png" alt="" height="200" width="200"></div>
+    </div>
+  </div>
+</div>
+</div>
+<?php include "foot.php" ?>
