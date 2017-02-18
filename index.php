1d9cd2575dcf7d1187503ddf42b2b28eb737c2fd
Aitthinat <aitthinat_s@cmu.ac.th>
2017-02-19 02:54:43 +0700

    Edit_HOME_PICTURE

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php include "head.php" ?>
 <div style="background:#d8e9f3">
-<div class="container">
-  <div class="row">
-    <div class="col align-self-start" style="background:black;width:">
-      <div><img class="img-responsive" src="img/rzm-61.png" alt="" height="200" width="200"></div>
-      <div><img class="img-responsive" src="img/rzm-6.png" alt="" height="200" width="200"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col align-self-start">
+      <div class="row">
+    <div class="col align-self-start" style="z-index: 100"><img class="img-responsive" src="img/1.png" align="right"></div>
     </div>
-    <div class="col align-self-center" style="background:white">
-    <img class="img-responsive" src="img/home.png" alt=""  height="100%" width="100%">
-    </div>
-    <div class="col align-self-end" style="background:black">
-      <div><img class="img-responsive" src="img/rzm-62.png" alt="" height="200" width="200"></div>
-      <div><img class="img-responsive" src="img/rzm-63.png" alt="" height="200" width="200"></div>
+    <div class="col align-self-start" style="z-index: 100"><img class="img-responsive" src="img/4.png" align="right"></div>
+      </div>
+      <div class="col align-self-center" style="margin-right: -60px;margin-left: -60px">
+    <div><center><img class="img-responsive" src="img/2.png"></center></div>
+    <div><center><img class="img-responsive" src="img/5.png"></center></div>
+      </div>
+      <div class="col align-self-end">
+    <div><img class="img-responsive" src="img/3.png" align="left"></div>
+    <div><img class="img-responsive" src="img/6.png" align="left"></div>
+      </div>
     </div>
   </div>
-</div>
 </div>
 <?php include "foot.php" ?>
